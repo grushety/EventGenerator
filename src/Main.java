edd3fc4040ff5a5c94abe914eea5edf1b65245eb
Yulia Grushetskaya <voyager13@list.ru>
2021-06-24 17:18:51 +0200

    Data Domain defined

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,25 @@
+import domain.StreamObject;
+
+import java.time.Clock;
+import java.time.Duration;
+import java.util.List;
+
 public class Main {
 
+    //Generation Parameters
+    int number_of_events = 100;  //for each Event Type
+    int out_of_order_quota = 30;
+    int negative_quota = 20;
+    int pog_quota = 20;
+    int number_of_events_type = 10;
+
     public static void main(String[] args) {
-	// write your code here
+
+        Clock c = Clock.systemUTC();
+        System.out.println(c.instant());
+        Duration d = Duration.ofHours(5);
+        Clock clock = Clock.offset(c, d);
+        System.out.println(clock.instant());
+
     }
 }
